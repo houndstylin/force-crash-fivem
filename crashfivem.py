983fac2ff23ab9b51b092cad0dd8e19230ad182d
Muddy <furryballa3@gmail.com>
2024-12-20 17:37:24 +0100

    Update crashfivem.py

--- crashfivem.py
+++ crashfivem.py
@@ -6,26 +6,32 @@ def crash_fivem():
         if proc.info['name'] == 'FiveM.exe':
             pid = proc.info['pid']
             print(f"[+] Found FiveM process [{pid}]")
-            PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF) # open the process handle
+
+            # open the process handle
+            PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
             process_handle = ctypes.windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
             
             if not process_handle:
-                print(f"could not open process: {ctypes.windll.kernel32.GetLastError()}")
+                print(f"Failed to open process: {ctypes.windll.kernel32.GetLastError()}")
                 return False
-            THREADFUNC = ctypes.WINFUNCTYPE(ctypes.c_ulong) # create a remote thread that points to an invalid addres
+            
+            # create a remote thread that points to an invalid addres
+            THREADFUNC = ctypes.WINFUNCTYPE(ctypes.c_ulong)
             pThreadFunc = THREADFUNC(0xDEADBEEF)
 
             thread_id = ctypes.c_ulong(0)
             if not ctypes.windll.kernel32.CreateRemoteThread(process_handle, None, 0, pThreadFunc, None, 0, ctypes.byref(thread_id)):
-                print(f"[-] Failed: {ctypes.windll.kernel32.GetLastError()}")
+                print(f"[-] Failed to create remote thread: {ctypes.windll.kernel32.GetLastError()}")
                 ctypes.windll.kernel32.CloseHandle(process_handle)
                 return False
             
             print("[+] Crashed Fivem")
-            ctypes.windll.kernel32.CloseHandle(process_handle) # close the process handle
+            
+            # close the process handle
+            ctypes.windll.kernel32.CloseHandle(process_handle)
             return True
     
-    print("[-] Couldnt find FiveM.")
+    print("FiveM.exe process not found.")
     return False
 
 if __name__ == "__main__":
